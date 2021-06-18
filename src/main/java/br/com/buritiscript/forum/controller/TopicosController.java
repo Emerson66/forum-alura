9f82e51e69a1a0e7ef4a2f75a88369a94c0b55bb
Emerson66 <emersonreisdacosta@gmail.com>
2021-06-18 15:50:51 -0400

    adicionado anotação @Controller e @ResponseBody

--- src/main/java/br/com/buritiscript/forum/controller/TopicosController.java
+++ src/main/java/br/com/buritiscript/forum/controller/TopicosController.java
@@ -3,14 +3,19 @@ package br.com.buritiscript.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.buritiscript.forum.model.Curso;
 import br.com.buritiscript.forum.model.Topico;
 
+@Controller
 public class TopicosController {
 	
 	@RequestMapping("/topicos")
+	@ResponseBody
+
 	public List<Topico> listar(){
 		
 		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
