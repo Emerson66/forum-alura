2a30e9246c2968c80d4274edac31be2391e2d56f
Emerson66 <emersonreisdacosta@gmail.com>
2021-06-18 15:39:49 -0400

    Criado o endpoint topicos

---
+++ src/main/java/br/com/buritiscript/forum/controller/TopicosController.java
@@ -0,0 +1,20 @@
+package br.com.buritiscript.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.buritiscript.forum.model.Curso;
+import br.com.buritiscript.forum.model.Topico;
+
+public class TopicosController {
+	
+	@RequestMapping("/topicos")
+	public List<Topico> listar(){
+		
+		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+		
+		return Arrays.asList(topico, topico, topico);
+	}
+}
