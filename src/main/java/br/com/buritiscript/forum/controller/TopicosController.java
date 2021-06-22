4640be1334b9bc01d624028671f215e37e5a2998
Emerson66 <emersonreisdacosta@gmail.com>
2021-06-22 11:52:01 -0400

    alterado o metodo listar para listar cursos especificos

--- src/main/java/br/com/buritiscript/forum/controller/TopicosController.java
+++ src/main/java/br/com/buritiscript/forum/controller/TopicosController.java
@@ -1,16 +1,12 @@
 package br.com.buritiscript.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.buritiscript.forum.controller.dto.TopicoDTO;
-import br.com.buritiscript.forum.model.Curso;
 import br.com.buritiscript.forum.model.Topico;
 import br.com.buritiscript.forum.repository.TopicoRepository;
 
@@ -20,10 +16,10 @@ public class TopicosController {
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")
-	public List<TopicoDTO> listar(){
+	@RequestMapping("/topicos/{nomeCurso}")
+	public List<TopicoDTO> listar(String nomeCurso){
 		
-		List<Topico> topicos = topicoRepository.findAll();
+		List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 		
 		return TopicoDTO.converter(topicos);
 	}
